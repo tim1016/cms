ff31c23486f1779dcbd780a6c91cd574c8a2d7b6
tim1016 <inkant@gmail.com>
2018-04-02 14:10:37 -0500

    Temporary Commit

--- includes/navigation.php
+++ includes/navigation.php
@@ -23,14 +23,15 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-            <?php
-                $query = "SELECT * FROM categories";
-                $query_select_all_categories = mysqli_query($connection, $query);
-                while($row = mysqli_fetch_assoc($query_select_all_categories)){
-                    $cat_title = $row['cat_title'];
-                    echo "<li><a href='#'>{$cat_title}</a></li>";
-                }
-            ?>
+                <?php
+                    $query = "SELECT * FROM categories";
+                    $query_select_all_categories = mysqli_query($connection, $query);
+                    while($row = mysqli_fetch_assoc($query_select_all_categories)){
+                        $cat_title = $row['cat_title'];
+                        echo "<li><a href='#'>{$cat_title}</a></li>";
+                    }
+                ?>
+                <li><a href="admin/index.php">Admin</a></li>    
             </ul>
         </div>
         <!-- /.navbar-collapse -->
