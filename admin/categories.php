e693a964a2717ec8cd66ffc30163aeed508fabad
tim1016 <inkant@gmail.com>
2018-04-02 20:56:45 -0500

    Added update category feature

--- admin/categories.php
+++ admin/categories.php
@@ -20,11 +20,18 @@
                         <?php 
                             if(isset($_POST['submit'])){
                                 $cat_title = $_POST['cat_title'];
-                                echo $cat_title;
+                                //echo $cat_title;
                                 if($cat_title == "" || empty($cat_title)) {
                                     echo "<p class='text-danger'> The category title cannot be an empty string</p>";
                                 }else{
-                                    
+                                    $query  = "INSERT INTO categories(cat_title) ";
+                                    $query .= "VALUE ('{$cat_title}')";
+
+                                    $query_create_category = mysqli_query($connection, $query);
+                                    if(!$query_create_category){
+                                        die("Create category Query failed with error: " . mysqli_error($connection));
+                                    }
+
                                 }
                             }
 
@@ -39,36 +46,83 @@
                                 <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
                         </form>
+
+                        <?php
+
+                            if(isset($_GET['edit'])){
+                                $the_cat_id=$_GET['edit']; 
+                        ?>
+
+                            <form action="" method="post">
+                                <div class="form-group">
+                                    <label for="cat_title">Update Category</label>
+                                    <input type="text" class="form-control" name="cat_title" id="cat_title">
+                                </div>
+
+                                <div class="form-group">
+                                    <input class="btn btn-primary" type="submit" name="update" value="Update">
+                                </div>
+                            </form>
+                        <?php
+                            if(isset($_POST['update'])){
+                                $update_title=$_POST['cat_title'];
+                                $query = "UPDATE categories SET cat_title='$update_title' WHERE cat_id=$the_cat_id";
+                                echo $query;
+                                $query_update_category = mysqli_query($connection, $query);
+                                if(!$query_update_category){
+                                    die("Update query failed : ". mysqli_error($connection));
+                                } 
+                                header('Location: categories.php'); 
+                            }
+                        
+                        }?>
+
+
+
                     </div>
 
                     <div class="col-xs-6">
-                        <?php
-                            $query = "SELECT * FROM categories LIMIT 30";
-                            $query_select_all_categories = mysqli_query($connection, $query);        
-                        ?>
-
-
-
-
 
                         <table class="table table-bordered table-hover">
                             <thead>
                                 <tr>
                                     <th>ID</th>
                                     <th>Category Title</th>
+                                    <th>Delete Category</th>
+                                    <th>Edit Category</th>
                                 </tr>
                             </thead>
 
                             <tbody>
-                                <?php
+                                <?php //Find all categories query
+                                    $query = "SELECT * FROM categories LIMIT 30";
+                                    $query_select_all_categories = mysqli_query($connection, $query); 
                                     while($row = mysqli_fetch_assoc($query_select_all_categories)){
                                         $cat_title = $row['cat_title'];
                                         $cat_id = $row['cat_id'];
                                         echo "<tr>";
                                         echo "<td> {$cat_id} </td>";
                                         echo "<td> {$cat_title} </td>";
+                                        echo "<td> <a href='categories.php?delete={$cat_id}'>&#x274E;  </a></td>";
+                                        echo "<td> <a href='categories.php?edit={$cat_id}'> &#x270E;</a></td>";
                                         echo "</tr>";
                                     }
+                                ?>
+    
+                                <?php
+                                // Delete query
+                                    if(isset($_GET['delete'])){
+                                        $the_cat_id=$_GET['delete'];
+                                        $query =  "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
+                                        $query_delete_category = mysqli_query($connection, $query);
+                                        if(!$query_delete_category){
+                                            die("DELETE category failed : " . mysqli_error($connection));
+                                        }
+                                        header("Location: categories.php");
+                                        //header("Refresh:0");
+                                    }
+                                
+                                
                                 ?>
                             </tbody>
                         </table>
